6bae4448cd966204a5d468eb80080b0746f463f5
Sahil pillania <sahilpillania88@gmail.com>
2023-01-13 11:45:44 +0530

    login signup component

---
+++ src/screens/loginSignup/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <Text>Signup screen</Text>
+    </View>
+  );
+};
+
+export default SignupScreen;
