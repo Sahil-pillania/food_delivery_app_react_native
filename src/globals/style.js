e983d927cacd920eba8491406889fdea004e7071
Sahil pillania <sahilpillania88@gmail.com>
2023-01-13 11:23:24 +0530

    Global style

---
+++ src/globals/style.js
@@ -0,0 +1,30 @@
+const module = {
+  colors: {
+    col1: "white",
+    col2: "red",
+    col3: "gray",
+  },
+  titles: {
+    title1: 30,
+    btntxt: 20,
+  },
+  btn1: {
+    width: "80%",
+    height: 50,
+    backgroundColor: "red",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 10,
+    color: "white",
+  },
+  hr80: {
+    width: "80%",
+    height: 1,
+    backgroundColor: "gray",
+    borderBottomWidth: 1,
+    marginVertical: 20,
+  },
+};
+
+export default module;
