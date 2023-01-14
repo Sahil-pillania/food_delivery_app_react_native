da811cf1b656005ddb7b722ee1f735b4d08d23db
Sahil pillania <sahilpillania88@gmail.com>
2023-01-14 10:19:33 +0530

    Navigation file

---
+++ src/screens/AuthNavigation.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomeScreen from "./WelcomeScreen";
+import SignupScreen from "./loginSignup/SignupScreen";
+import LoginScreen from "./loginSignup/LoginScreen";
+
+const AuthNavigation = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator initialRouteName="Welcomepage">
+      <Stack.Screen
+        name="Welcomepage"
+        component={WelcomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="signup"
+        component={SignupScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigation;
