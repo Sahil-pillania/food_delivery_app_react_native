7d244311b31e9b49b4ff26f46d75b55843d265c1
Sahil pillania <sahilpillania88@gmail.com>
2023-01-14 11:14:18 +0530

    navigation

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import RootNavigation from "./src/RootNavigation";
 import LoginScreen from "./src/screens/loginSignup/LoginScreen";
+import SignupScreen from "./src/screens/loginSignup/SignupScreen";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
 
 export default function App() {
   return (
     <>
       {/* <WelcomeScreen /> */}
-      <LoginScreen />
+      <RootNavigation />
+      {/* <LoginScreen /> */}
+      {/* <SignupScreen/> */}
     </>
   );
 }
