422ff03a2d7713b5c53390e857ace1815bdc0302
Sahil pillania <sahilpillania88@gmail.com>
2023-01-16 10:15:04 +0530

    components

---
+++ src/components/HomeHead.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeHead = () => {
+  return (
+    <View>
+      <Text>HomeHead</Text>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({});
+export default HomeHead;
