db9a6ae4a8479602527c2b0ac6a0d81c94811828
Sahil pillania <sahilpillania88@gmail.com>
2023-01-17 13:12:12 +0530

    app

--- data_fill_app/src/App.js
+++ data_fill_app/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import AddFoodData from "./components/AddFoodData";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container" style={{ margin: "2rem" }}>
+      <AddFoodData />
+    </div>
+  );
 }
 
 export default App;
